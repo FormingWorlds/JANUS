5257276d0334bc42bda10046be3dbeb99f0ff410
Laurent Soucasse <135331272+lsoucasse@users.noreply.github.com>
2024-07-11 12:43:41 +0200

    Create shortcut to access Janus functions.

---
+++ src/janus/modules/__init__.py
@@ -0,0 +1,8 @@
+#Main Janus solvers 
+from .solve_pt import RadConvEqm, MCPA, MCPA_CBL
+
+#Plot fluxes
+from .plot_flux_balance import plot_fluxes
+
+#Plot emission
+from .plot_emission_spectrum import plot_emission
