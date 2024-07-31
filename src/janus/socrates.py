c871fd1213eae698fd8d66b68910796913ff2cf2
Stef Smeets <s.smeets@esciencecenter.nl>
2024-07-31 16:10:41 +0200

    Set default directory

--- src/janus/socrates.py
+++ src/janus/socrates.py
@@ -1,9 +1,11 @@
 import os
-from pathlib import Path
 import sys
+from pathlib import Path
 
-FWL_DATA = Path(os.environ.get('FWL_DATA'))
-SOCRATES_DIR = FWL_DATA / 'socrates'
+import platformdirs
+
+SOCRATES_DIR = Path(os.environ.get('SOCRATES', platformdirs.user_data_dir('socrates')))
+
 
 def set_rad_env():
     """Set environment variables for socrates.
@@ -11,11 +13,13 @@ def set_rad_env():
     Based on:
     https://github.com/FormingWorlds/SOCRATES/blob/main/sbin/set_rad_env_tmp
     """
-    assert SOCRATES_DIR.exists()
-    
+    if not SOCRATES_DIR.exists():
+        raise RuntimeError(f'Cannot find SOCRATES in this location: {SOCRATES_DIR}')
+
     with open(SOCRATES_DIR / 'version') as f:
         version = f.readline()
 
+    print(f'socrates location: {SOCRATES_DIR}')
     print(f'socrates version: {version}')
 
     sep = os.pathsep
@@ -27,7 +31,7 @@ def set_rad_env():
     os.environ['LOCK_FILE'] = 'radiation_code.lock'
     os.environ['PATH'] = str(SOCRATES_DIR / 'bin') + sep + os.environ['PATH']
     os.environ['PATH'] = str(SOCRATES_DIR / 'sbin') + sep + os.environ['PATH']
-    os.environ['MANPATH'] = str(SOCRATES_DIR / 'man') + sep + os.environ.get('MANPATH', '') 
+    os.environ['MANPATH'] = str(SOCRATES_DIR / 'man') + sep + os.environ.get('MANPATH', '')
     sys.path.append(str(SOCRATES_DIR / 'python'))
 
     os.environ['LD_LIBRARY_PATH'] = 'netcdfff' + sep + os.environ.get('LD_LIBRARY_PATH', '')
