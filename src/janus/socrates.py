d7476a926e21a0a44d01bf5d223626528775cf85
Stef Smeets <s.smeets@esciencecenter.nl>
2024-08-01 16:40:46 +0200

    Clean download code

--- src/janus/socrates.py
+++ src/janus/socrates.py
@@ -1,13 +1,13 @@
+from __future__ import annotations
+
 import os
 import sys
+import zipfile
 from pathlib import Path
 
 import click
 import platformdirs
 import requests
-
-import zipfile
-
 
 SOCRATES_DIR = Path(os.environ.get('SOCRATES', platformdirs.user_data_dir('socrates')))
 
@@ -42,17 +42,24 @@ def set_rad_env():
     os.environ['LD_LIBRARY_PATH'] = 'netcdfff' + sep + os.environ.get('LD_LIBRARY_PATH', '')
 
 
-def download_socrates():
-    filename = 'socrates.zip'
-    version = 'x.y.z'
-    url = f'https://github.com/FormingWorlds/SOCRATES/releases/download/{version}/socrates-Linux.zip'
+def _set_permissions(drc: Path):
+    """Set executable flag for scripts."""
+    for p in drc.iterdir():
+        if p.suffix:
+            continue
+        if p.stem.startswith('Mk_') or p.stem.startswith('Make'):
+            continue
+        # set executable flag
+        p.chmod(mode=33261)
+
+
+def _download(*, url: str, filename: str):
+    """Download file from url."""
+    chunk_size = 1024 * 8
 
     with requests.get(url, stream=True) as r:
-        total_size = int(r.headers.get('Content-Length', 0))
-        chunk_size = 1024 * 8
-
         r.raise_for_status()
-
+        total_size = int(r.headers.get('Content-Length', 0))
         with (
             open(filename, 'wb') as f,
             click.progressbar(label='Downloading', length=total_size) as pbar,
@@ -61,15 +68,28 @@ def download_socrates():
                 f.write(chunk)
                 pbar.update(chunk_size)
 
+
+def download_socrates():
+    version = 'x.y.z'
+    filename = 'socrates.zip'
+
+    _download(
+        url=f'https://github.com/FormingWorlds/SOCRATES/releases/download/{version}/socrates-Linux.zip',
+        filename=filename,
+    )
+
     SOCRATES_DIR.mkdir(exist_ok=True, parents=True)
 
-    target_dir = SOCRATES_DIR / 'socrates'
-
-    if target_dir.exists():
-        raise RuntimeError(f'SOCRATES directory already exists: {SOCRATES_DIR}')
+    is_empty = not any(SOCRATES_DIR).iterdir()
+    if not is_empty:
+        raise RuntimeError(f'SOCRATES directory is not empty: {SOCRATES_DIR}')
 
     with zipfile.ZipFile(filename, 'r') as zip_ref:
         zip_ref.extractall(SOCRATES_DIR)
 
-    print(f'SOCRATES downloaded to {SOCRATES_DIR}')
+    _set_permissions(SOCRATES_DIR / 'bin')
+    _set_permissions(SOCRATES_DIR / 'sbin')
+
     filename.unlink()
+
+    print(f'SOCRATES downloaded to {SOCRATES_DIR}')
