97ab299d7134dc80af43567cc6a25d9ba872f5ab
Stef Smeets <stefsmeets@users.noreply.github.com>
2024-08-01 09:03:01 +0200

    Refactor and speed up tests (#41)
    * Add fwl_data to .gitignore
    
    * Clean test files
    
    * Move env check to conftest
    
    * Avoid updating class variable
    
    * Clean paths and use tmp directory for output
    
    * Avoid running the same tests twice in a PR
    
    * Refactos greenhous test
    
    * Refactor instellation test
    
    * Clean imports and run code in tmpdir
    
    * Set absolute path for FWL_DATA
    
    * Fix download
    
    * Parametrize tests
    
    * Limit number of tests
    
    * Update tests/test_runaway_greenhouse.py
    
    Co-authored-by: Laurent Soucasse <135331272+lsoucasse@users.noreply.github.com>
    
    * Update tests/test_instellation.py
    
    Co-authored-by: Laurent Soucasse <135331272+lsoucasse@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Laurent Soucasse <135331272+lsoucasse@users.noreply.github.com>

---
+++ tests/helpers/__init__.py
@@ -0,0 +1,73 @@
+import os
+from contextlib import contextmanager
+from importlib.resources import files
+from pathlib import Path
+
+import mors
+import toml
+from janus.utils import (
+    DownloadSpectralFiles,
+    DownloadStellarSpectra,
+    ReadBandEdges,
+    StellarSpectrum,
+    atmos,
+)
+
+DATA_DRC = files('janus') / 'data' / 'tests'
+FWL_DATA = os.environ.get('FWL_DATA')
+
+
+@contextmanager
+def work_directory(path: Path | str):
+    """Changes working directory and returns to previous on exit.
+
+    Parameters
+    ----------
+    path : Path | str
+        Temporarily change to this directory.
+    """
+    prev_cwd = Path.cwd().resolve()
+    try:
+        os.chdir(path)
+        yield
+    finally:  # In any case, no matter what happens, go back eventually
+        os.chdir(prev_cwd)
+
+
+def get_spectrum_data(drc):
+    DownloadSpectralFiles('Oak')
+    DownloadStellarSpectra()
+
+    spec = mors.Spectrum()
+    spec.LoadTSV(FWL_DATA + '/stellar_spectra/Named/sun.txt')
+    socstar = os.path.join(drc, 'socstar.txt')
+    StellarSpectrum.PrepareStellarSpectrum(spec.wl, spec.fl, socstar)
+
+    StellarSpectrum.InsertStellarSpectrum(
+        FWL_DATA + '/spectral_files/Oak/318/Oak.sf',
+        socstar,
+        drc,
+    )
+    band_edges = ReadBandEdges(drc + 'star.sf')
+
+    return band_edges
+
+
+def get_atmosphere_config(*, band_edges, cfg_name: str, distance: float):
+    cfg_file = DATA_DRC / cfg_name
+
+    with open(cfg_file) as f:
+        cfg = toml.load(f)
+
+    star_time = cfg['star']['time']
+    star_mass = cfg['star']['star_mass']
+
+    vol_mixing = {'H2O': 1.0, 'CO2': 0.0, 'N2': 0.0}
+
+    atm = atmos.from_file(cfg_file, band_edges, vol_mixing=vol_mixing, vol_partial={})
+
+    mors.DownloadEvolutionTracks('/Baraffe')
+    baraffe = mors.BaraffeTrack(star_mass)
+    atm.instellation = baraffe.BaraffeSolarConstant(star_time, distance)
+
+    return atm, cfg
